aee1c3119d7337166e5fec95f3d6843cfc62c229
jdbdev <juliendb.dev@gmail.com>
2025-02-07 19:09:04 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+tutorial:https://www.youtube.com/watch?v=aLVJY-1dKz8&t=368s
+*/
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("go-crud")
+}
